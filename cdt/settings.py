cf8ec1ee533a8fdd563529fd8206fe30d3008c9c
Pylearn2017 <alexlik73@gmail.com>
2019-06-14 20:06:52 +0300

    Change settings

--- cdt/settings.py
+++ cdt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yg8!99!on$im$j#934_k%d)_pddoo=#s!h0@rsa4c++tssj6cd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'python3strogino.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
